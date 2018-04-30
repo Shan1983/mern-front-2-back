38ff342e572bfba8e335628d9ca4449061e9c97d
Shan1983 <s.galbraith08@gmail.com>
2018-04-30 22:50:38 +1000

    set up create a post, get a single post, and delete a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 const { validatePostInput } = require('../../validation/post');
 
@@ -36,6 +37,60 @@ router.post(
       .save()
       .then(post => res.json(post))
       .catch(err => res.status(400).json({ post: 'Failed to post.' }));
+  },
+);
+
+// @route GET api/posts
+// @desc get all posts
+// @access public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(400).json({ nopostsfound: 'No posts found.' }));
+});
+
+// @route GET api/posts/:post_id
+// @desc get posts by id
+// @access public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(400).json({ nopostfound: 'No post with that id found.' }),
+    );
+});
+
+// @route DELETE api/posts/:id
+// @desc delete posts by id
+// @access private
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            if (post.user.toString() !== req.user.id) {
+              return res
+                .status(401)
+                .json({ noauthorized: 'User not authorized.' });
+            }
+            post
+              .remove()
+              .then(() => res.json({ success: true }))
+              .catch(err =>
+                res.status(404).json({ postnotfound: 'No post found.' }),
+              );
+          })
+          .catch(err =>
+            res.status(400).json({ nopostfound: 'No Post found.' }),
+          );
+      })
+      .catch(err =>
+        res.status(400).json({ noprofilefound: 'No profile found.' }),
+      );
   },
 );
 
