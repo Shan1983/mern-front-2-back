a155370cd099e20dd47971c51ccc283bd12e555b
Shan1983 <s.galbraith08@gmail.com>
2018-05-04 08:51:09 +1000

    set up the navbar, landing, and footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Happy joy</h1>
+        <Navbar />
+        <div>
+          <Landing />
+        </div>
+        <Footer />
       </div>
     );
   }
