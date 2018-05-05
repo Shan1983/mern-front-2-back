bfea221baa8a1ddb676c7aee247202fdc458615c
Shan1983 <s.galbraith08@gmail.com>
2018-05-05 20:59:57 +1000

    implemented redux for the login component

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,9 @@
-import { GET_ERRORS } from './types';
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
-// test
+// register user action
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post(`api/users/register`, userData)
@@ -12,4 +14,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data,
       }),
     );
+};
+
+// login user action - get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post(`/api/users/login`, userData)
+    .then(response => {
+      // save token
+      const { token } = response.data;
+      // set up local storage and save token
+      localStorage.setItem('jwtToken', token);
+      // set up the token to the auth header
+      setAuthToken(token);
+      // decode the token to get the user data
+      const decoded = jwt_decode(token);
+      // set the current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      }),
+    );
+};
+
+// set up the current user
+export const setCurrentUser = data => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: data,
+  };
 };
